298b781fa3fd087535792819936d401626b0fbc9
Jose Miguel Dueñez Palomo <jduenezp@torreon.coah>
2022-08-02 14:27:10 -0500

    Nuevos Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
-import 'package:band_names/pages/home.dart';
+import 'package:band_names/sevices/socket_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:band_names/pages/home.dart';
+import 'package:band_names/pages/status.dart';
+import 'package:provider/single_child_widget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomeScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SocketService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'status',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'status': (_) => StatusScreen(),
+        },
+      ),
     );
   }
 }
---
+++ lib/sevices/socket_service.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+
+  SocketService() {
+    this._initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+    IO.Socket socket = IO.io('http://192.168.50.21:3000/', {
+      'transports': ['websocket'],
+      'autoConnect': true
+    });
+
+    socket.onConnect((_) {
+      print('conectado');
+      socket.emit('msg', 'test');
+    });
+    socket.on('event', (data) => print(data));
+    socket.onDisconnect((_) => print('disconnect'));
+    socket.on('fromServer', (_) => print(_));
+  }
+}
